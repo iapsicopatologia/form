08746e07ee9d65c7c1c23626b5ed30cd4a396acd
iapsicopatologia <110908433+iapsicopatologia@users.noreply.github.com>
2022-10-01 18:09:06 -0300

    Versão 5.4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -322,9 +322,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "73a1b7ce5c1aef9793dd669082121b3a",
-"/": "73a1b7ce5c1aef9793dd669082121b3a",
-"main.dart.js": "9f3a79c2ea269b5f14d3bc0a865a9e59",
+"index.html": "9e8ef658fa667510b22d97e3fed53f5e",
+"/": "9e8ef658fa667510b22d97e3fed53f5e",
+"main.dart.js": "cb65c2c551b3c48840ecb739812e4f95",
 "manifest.json": "366d24dab98978321bf2af740356be97",
 "version.json": "987471763695800e243621872bc3a279"
 };
